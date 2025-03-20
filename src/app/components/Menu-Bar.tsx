c87b7b983a860f63a3c23a63d3b0874e194991d4
Andres de Jesus Baez Medina <89228939+AndresBaMe@users.noreply.github.com>
2025-03-20 12:39:51 -0600

    Se agrego el diseño base del menu

---
+++ src/app/components/Menu-Bar.tsx
@@ -0,0 +1,32 @@
+
+'use client'
+
+import Image from 'next/image'
+export default function Menubar(){
+    const handleClick=()=>{
+        alert('Hola')
+    }
+
+
+
+    return (
+        <div style={{ boxShadow: "0px 0px 25px 1px #000" }} className=" bg-[#211C84] rounded-b-[10] flex w-auto h-[100px] p-[16px_30px] justify-between items-center flex-shrink-0">
+            <input type="text" placeholder="Buscar..." className="font-bold font-mono placeholder-black w-[930] h-[30] bg-[#ffff] rounded-[10] p-[10]"/>
+            <select className='font-mono bg-white text-black rounded-[10] p-[5]'>
+                <option value="option1">option1</option>
+                <option value="option2">option2</option>
+                <option value="option3">option3</option>
+            </select>
+            <button className='bg-white rounded-[10] p-[5] cursor-pointer' onClick={handleClick}>
+                   <Image
+                          className="border-b-2 w-auto h-auto max-w-[180px]"
+                          src="/cartIcon.svg"
+                          alt="Shoping cart icon"
+                          width={61}
+                          height={61}
+                          priority
+                        />
+            </button>
+        </div>
+    )
+}
