e3eff47d20011dcb448c92364635333b9e5584e9
Andres de Jesus Baez Medina <89228939+AndresBaMe@users.noreply.github.com>
2025-03-21 00:40:41 -0600

    Se termino el diseño del menu

--- src/app/components/Menu-Bar.tsx
+++ src/app/components/Menu-Bar.tsx
@@ -6,12 +6,19 @@ export default function Menubar(){
     const handleClick=()=>{
         alert('Hola')
     }
-
-
-
     return (
         <div style={{ boxShadow: "0px 0px 25px 1px #000" }} className=" bg-[#211C84] rounded-b-[10] flex w-auto h-[100px] p-[16px_30px] justify-between items-center flex-shrink-0">
-            <input type="text" placeholder="Buscar..." className="font-bold font-mono placeholder-black w-[930] h-[30] bg-[#ffff] rounded-[10] p-[10]"/>
+            <div className='bg-[#ffff] rounded-[10] flex items-center'>
+                <input type="text" placeholder="Buscar..." className="font-bold font-mono text-black placeholder-black w-[930] h-[30] p-[10] focus:outline-none"/>
+                    <Image
+                          className="border-b-2 cursor-pointer"
+                          src="/searchIcon.svg"
+                          alt="Shoping cart icon"
+                          width={40}
+                          height={40}
+                          priority
+                    />
+            </div>
             <select className='font-mono bg-white text-black rounded-[10] p-[5]'>
                 <option value="option1">option1</option>
                 <option value="option2">option2</option>
